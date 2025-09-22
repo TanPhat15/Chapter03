21f75defaf98569e0c78e677498233053499e088
TanPhat15 <126132958+TanPhat15@users.noreply.github.com>
2025-09-22 14:30:37 +0700

    Update Program.cs

--- CastingConverting/Program.cs
+++ CastingConverting/Program.cs
@@ -1,150 +1,188 @@
-﻿using static System.Convert; // To use the ToInt32 method.
-using System.Globalization; // To use CultureInfo.
+using System;
+using System.Data;
+using System.Globalization;
+using static System.Convert;
 
-#region Casting numbers implicitly and explicitly
-
-int a = 10;
-double b = a; // An int can be safely cast into a double.
-WriteLine($"a is {a}, b is {b}");
-
-double c = 9.8;
-int d = (int)c; // Compiler gives an error if you do not explicitly cast.
-WriteLine($"c is {c}, d is {d}"); // d loses the .8 part.
-
-long e = 5_000_000_000;
-int f = (int)e;
-WriteLine($"e is {e:N0}, f is {f:N0}");
-
-e = long.MaxValue;
-f = (int)e;
-WriteLine($"e is {e:N0}, f is {f:N0}");
-
-#endregion
-
-#region How negative numbers are represented in binary
-
-WriteLine("{0,12} {1,34}", "Decimal", "Binary");
-WriteLine("{0,12} {0,34:B32}", int.MaxValue);
-for (int i = 8; i >= -8; i--)
+namespace chapter3
 {
-  WriteLine("{0,12} {0,34:B32}", i);
+    class implicitEx
+    {
+        public static void Run()
+        {
+            int a = 10;
+            double b = a;
+            Console.WriteLine($"a is {a}, b is {b}");
+
+            //double c = 9.8;
+            //int d = c ;
+            //Console.WriteLine($"c is {c}, d is {d}");
+        }
+    }
+
+    class explicitEx
+    {
+        public static void Run()
+        {
+            double a = 9.8;
+            int b = (int)a;
+            Console.WriteLine($"a is {a}, b is {b}");
+
+            long e = long.MaxValue;
+            int f = (int)e;
+            Console.WriteLine($"e is {e}, f is {f}");
+        }
+    }
+
+    class ConvertEx
+    {
+        public static void Run()
+        {
+            double a = 9.8;
+            int b = ToInt32(a);
+            Console.WriteLine($"a is {a}, b is {b}");
+        }
+    }
+
+    class ToStringEx
+    {
+        public static void Run()
+        {
+            int number = 12;
+            Console.WriteLine(number.ToString());
+            bool boolean = true;
+            Console.WriteLine(boolean.ToString());
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now.ToString());
+            object me = new();
+            Console.WriteLine(me.ToString());
+        }
+    }
+
+    class RoundingNumbers()
+    {
+        public static void Run()
+        {
+            double[,] doubles = {
+                                            { 9.49  , 9.5  , 9.51   },
+                                            { 10.49 , 10.5 , 10.51  },
+                                            { 11.49 , 11.5 , 11.51  },
+                                            { 12.49 , 12.5 , 12.51  },
+                                            { -12.49, -12.5, -12.51 },
+                                            { -11.49, -11.5, -11.51 },
+                                };
+            foreach (double n in doubles)
+            {
+                Console.WriteLine(format:
+                  "Math.Round({0}, 0, MidpointRounding.AwayFromZero) is {1}",
+                  arg0: n,
+                  arg1: Math.Round(value: n, digits: 0,
+                          mode: MidpointRounding.AwayFromZero));
+            }
+        }
+    }
+
+    class Base64Ex()
+    {
+        public static void Run()
+        {
+            // Allocate an array of 128 bytes.
+            byte[] binaryObject = new byte[128];
+
+            // Populate the array with random bytes.
+            Random.Shared.NextBytes(binaryObject);
+
+            Console.WriteLine("Binary Object as bytes:");
+            for (int index = 0; index < binaryObject.Length; index++)
+            {
+                Console.Write($"{binaryObject[index]:X2} ");
+            }
+            Console.WriteLine();
+
+            // Convert the array to Base64 string and output as text.
+            string encoded = ToBase64String(binaryObject);
+            Console.WriteLine($"Binary Object as Base64: {encoded}");
+        }
+    }
+
+    class ParseEx()
+    {
+        public static void Run()
+        {
+            // Set the current culture to make sure date parsing works.
+            CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+
+            int friends = int.Parse("27");
+            DateTime birthday = DateTime.Parse("4 June 1980");
+            Console.WriteLine($"I have {friends} friends to invite to my party.");
+            Console.WriteLine($"My birthday is {birthday}.");
+            Console.WriteLine($"My birthday is {birthday:D}.");
+        }
+    }
+
+    class TryParseEx()
+    {
+        public static void Run()
+        {
+            Console.Write("How many eggs are there? ");
+            string? input = Console.ReadLine();
+
+            if (int.TryParse(input, out int count))
+            {
+                Console.WriteLine($"There are {count} eggs.");
+            }
+            else
+            {
+                Console.WriteLine("I could not parse the input.");
+            }
+        }
+    }
+
+    class checkedEx()
+    {
+        public static void Run()
+        {
+            checked
+            {
+                int x = int.MaxValue - 1;
+                Console.WriteLine($"Initial : {x}");
+                x++;
+                Console.WriteLine($"Initial : {x}");
+                x++;
+                Console.WriteLine($"Initial : {x}");
+            }
+        }
+    }
+
+    class UncheckedEx()
+    {
+        public static void Run()
+        {
+            unchecked
+            {
+                long x = long.MaxValue + 1;
+                Console.WriteLine($"Initial : {x}");
+                x--;
+                Console.WriteLine($"Initial : {x}");
+                x--;
+                Console.WriteLine($"Initial : {x}");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            implicitEx.Run();
+            explicitEx.Run();
+            ConvertEx.Run();
+            ToStringEx.Run();
+            RoundingNumbers.Run();
+            Base64Ex.Run();
+            ParseEx.Run();
+            TryParseEx.Run();
+            checkedEx.Run();
+            UncheckedEx.Run();
+        }
+    }
 }
-WriteLine("{0,12} {0,34:B32}", int.MinValue);
-
-long r = 0b_101000101010001100100111010100101010;
-int s = (int)r;
-
-WriteLine($"{r,38:B38} = {r}");
-WriteLine($"{s,38:B32} = {s}");
-
-#endregion
-
-#region Converting with the System.Convert type
-
-double g = 9.8;
-int h = ToInt32(g); // A method of System.Convert.
-WriteLine($"g is {g}, h is {h}");
-
-#endregion
-
-#region Rounding numbers and the default rounding rules
-
-double[,] doubles = {
-  { 9.49, 9.5, 9.51 },
-  { 10.49, 10.5, 10.51 },
-  { 11.49, 11.5, 11.51 },
-  { 12.49, 12.5, 12.51 },
-  { -12.49, -12.5, -12.51 },
-  { -11.49, -11.5, -11.51 },
-  { -10.49, -10.5, -10.51 },
-  { -9.49, -9.5, -9.51 }
-};
-
-WriteLine($"| double | ToInt32 | double | ToInt32 | double | ToInt32 |");
-for (int x = 0; x < 8; x++)
-{
-  for (int y = 0; y < 3; y++)
-  {
-    Write($"| {doubles[x, y],6} | {ToInt32(doubles[x, y]),7} ");
-  }
-  WriteLine("|");
-}
-WriteLine();
-
-#endregion
-
-#region Taking control of rounding rules
-
-foreach (double n in doubles)
-{
-  WriteLine(format:
-    "Math.Round({0}, 0, MidpointRounding.AwayFromZero) is {1}",
-    arg0: n,
-    arg1: Math.Round(value: n, digits: 0,
-            mode: MidpointRounding.AwayFromZero));
-}
-
-#endregion
-
-#region Converting from any type to a string
-
-int number = 12;
-WriteLine(number.ToString());
-bool boolean = true;
-WriteLine(boolean.ToString());
-DateTime now = DateTime.Now;
-WriteLine(now.ToString());
-object me = new();
-WriteLine(me.ToString());
-
-#endregion
-
-#region Converting from a binary object to a string
-
-// Allocate an array of 128 bytes.
-byte[] binaryObject = new byte[128];
-
-// Populate the array with random bytes.
-Random.Shared.NextBytes(binaryObject);
-
-WriteLine("Binary Object as bytes:");
-for (int index = 0; index < binaryObject.Length; index++)
-{
-  Write($"{binaryObject[index]:X2} ");
-}
-WriteLine();
-
-// Convert the array to Base64 string and output as text.
-string encoded = ToBase64String(binaryObject);
-WriteLine($"Binary Object as Base64: {encoded}");
-
-#endregion
-
-#region Parsing from strings to numbers or dates and times
-
-// Set the current culture to make sure date parsing works.
-CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
-
-int friends = int.Parse("27");
-DateTime birthday = DateTime.Parse("4 June 1980");
-WriteLine($"I have {friends} friends to invite to my party.");
-WriteLine($"My birthday is {birthday}.");
-WriteLine($"My birthday is {birthday:D}.");
-
-#endregion
-
-#region Avoiding Parse exceptions by using the TryParse method
-
-Write("How many eggs are there? ");
-string? input = ReadLine();
-
-if (int.TryParse(input, out int count))
-{
-  WriteLine($"There are {count} eggs.");
-}
-else
-{
-  WriteLine("I could not parse the input.");
-}
-
-#endregion

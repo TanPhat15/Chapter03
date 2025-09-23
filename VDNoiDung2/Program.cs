f5804a554070e3183e014199a2aa73e95e2420d3
TanPhat15 <phatran2k3@gmail.com>
2025-09-23 07:37:02 +0700

    .

---
+++ VDNoiDung2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using System.Globalization;
+using static System.Convert;
+
+namespace VDNoiDung2
+{
+    class ConvertQuestion
+    {
+        public static void Run()
+        {
+            double g = 9.5;
+            int h = Convert.ToInt32(g);
+            Console.WriteLine($"g = {g}, h = {h}");
+        }
+    }
+
+    class CheckedQuestion
+    {
+        public static void Run()
+        {
+            checked
+            {
+                int a = int.MaxValue;
+                a++;
+                Console.WriteLine(a);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConvertQuestion.Run();
+            CheckedQuestion.Run();
+        }
+    }
+}
